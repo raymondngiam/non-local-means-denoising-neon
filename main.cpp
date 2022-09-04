25d34208fecdea763559c6a6dea7c6b4f9fde13a
raymondngiam <ha.ngiam87@gmail.com>
2022-09-04 20:44:45 +0800

    Add src

---
+++ main.cpp
@@ -0,0 +1,306 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <fstream>
+#include <cassert>
+#include <sstream>
+#include <cmath>
+#include <algorithm>
+#include <thread>
+#include <arm_neon.h>
+
+// utility string function
+std::vector<std::string> split (std::string s, char delim) {
+    std::vector<std::string> result;
+    std::stringstream ss(s);
+    std::string item;
+    while (getline (ss, item, delim)) {
+        result.push_back (item);
+    }
+    return result;
+}
+
+// custom image class to read and write ascii pgm image
+template <typename T>
+struct Image{
+    int height;
+    int width;
+    std::vector<T> data;
+    Image() = default;
+    Image(int height, int width){
+        this->height=height;
+        this->width=width;
+        data = std::vector<T>(height*width,0);
+    }
+    bool read(std::string filename){
+        std::string buffer;
+        std::ifstream f(filename);
+        std::getline(f,buffer);
+        assert(buffer=="P2" && "Wrong file format");
+        std::getline(f,buffer);
+        std::vector<std::string> splitted = split(buffer,' ');
+        assert(splitted.size()==2 && "Wrong image size entry. Shape dimension not equal (2,)");
+        height = std::stoi(splitted[0]);
+        width = std::stoi(splitted[1]);
+        std::cout<<"Loading image of shape "<<height<<"[Height] x "<<width<<"[Width]"<<std::endl;
+        std::getline(f,buffer); //discard `MaxVal` line
+        for (std::string line; std::getline(f, line); )
+        {
+            splitted = split(line,' ');
+            for (const auto& val : splitted){
+                if(val.find_first_not_of(' ') != std::string::npos) {
+                    int parsedInt = std::stoi(val);
+                    data.push_back((T) parsedInt);
+                }
+            }
+        }
+        std::cout<<"Data loaded ["<<data.size()<<" bytes]"<<std::endl;
+        if (data.size()==width*height){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    bool write(std::string filename){
+        std::ofstream f(filename);
+        f << "P2\n";
+        f << height << "  " << width << "\n";
+        f << "255\n";
+        for (int r = 0; r < height; r++) {
+            std::stringstream ss;
+            ss << std::to_string(data[r * height]);
+            for (int c = 1; c < width; c++) {
+                ss << "  " << std::to_string(data[r * height + c]);
+            }
+            f << ss.str() << std::endl;
+        }
+        return true;
+    }
+};
+
+const int SIMD_MULTPLE = 4;
+
+// neon simd utility function
+float ssd_reduce(const float* ptrA, const float* ptrB, uint32_t count) {
+    int remainder = count % SIMD_MULTPLE;
+    int fullLoopCount = count/SIMD_MULTPLE; //floor
+    int fullLoopEnd = (fullLoopCount-1)*SIMD_MULTPLE ;
+
+    float32x2_t vec64a, vec64b;
+    float32x4_t vec128 = vdupq_n_f32(0.0); // clear accumulators
+    float32x4_t vecA, vecB;
+
+    // full stride, contiguous memory access loop
+    for (int i = 0; i <= fullLoopEnd; i+=SIMD_MULTPLE) {
+        vecA = vld1q_f32(ptrA+i); // load four 32-bit values
+        vecB = vld1q_f32(ptrB+i); // load four 32-bit values
+        float32x4_t diff = vsubq_f32(vecA,vecB);
+        float32x4_t squared = vmulq_f32(diff,diff);
+        vec128=vaddq_f32(vec128, squared); // accumulate the squared_diff
+    }
+    // remainder loop
+    if(remainder != 0){
+        int remainderFirstElement = (fullLoopCount)*SIMD_MULTPLE;
+        vecA = vld1q_f32(ptrA+remainderFirstElement); // load four 32-bit values
+        vecB = vld1q_f32(ptrB+remainderFirstElement); // load four 32-bit values
+
+        // set remainder to 0
+        switch (remainder) {
+            case 3:
+                vsetq_lane_f32(0,vecA,1);
+                vsetq_lane_f32(0,vecB,1);
+            case 2:
+                vsetq_lane_f32(0,vecA,2);
+                vsetq_lane_f32(0,vecB,2);
+            case 1:
+            default:
+                vsetq_lane_f32(0,vecA,3);
+                vsetq_lane_f32(0,vecB,3);
+                break;
+        }
+
+        float32x4_t diff = vsubq_f32(vecA,vecB);
+        float32x4_t squared = vmulq_f32(diff,diff);
+        vec128=vaddq_f32(vec128, squared); // accumulate the squared_diff
+
+    }
+    vec64a = vget_low_f32(vec128); // split 128-bit vector
+
+    vec64b = vget_high_f32(vec128); // into two 64-bit vectors
+
+    vec64a = vadd_f32 (vec64a, vec64b); // add 64-bit vectors together
+
+    float result = vget_lane_f32(vec64a, 0); // extract lanes and
+
+    result += vget_lane_f32(vec64a, 1); // add together scalars
+
+    return result;
+}
+
+void nlm_unoptimized(int N,
+                     int K,
+                     float h,
+                     int padLen,
+                     int threadCount,
+                     const Image<float> &padded,
+                     Image<float> &output,
+                     Image<float> &B);
+
+void nlm_neon(int N,
+              int K,
+              float h,
+              int padLen,
+              int threadCount,
+              const Image<float> &padded,
+              Image<float> &output,
+              Image<float> &B);
+
+int main(int argc, char** argv) {
+    int option = 0;
+    int threadCount=1;
+    if(argc>=2){
+        if (std::stoi(argv[1])==1) {
+            option = 1;
+            std::cout << "Neon optimized version" << std::endl;
+        }
+        else
+        {
+            std::cout << "Unoptimized version" << std::endl;
+        }
+    }
+    if(argc==3){
+        threadCount = std::thread::hardware_concurrency();
+    }
+
+    // Read input
+    Image<uint8_t> im_byte;
+    if(im_byte.read("./lena_128x128_sigma_0.2.ascii.pgm")){
+        std::cout<<"Image loaded"<<std::endl;
+    }
+    else{
+        std::cout<<"Failed to load image"<<std::endl;
+    }
+    Image<float> im(im_byte.height,im_byte.width);
+    std::transform(im_byte.data.begin(),
+                   im_byte.data.end(),
+                   im.data.begin(),
+                   [](auto i){ return i/255.0; }
+                  );
+
+    auto start = std::chrono::high_resolution_clock::now();
+    // TODO
+    int N=9;
+    int K=3;
+    float h=0.2;
+    int padLen= N+K;
+    Image<float> output(im.height,im.width);
+    Image<float> C(im.height,im.width);
+    Image<float> padded(im.height + 2*padLen,im.width + 2*padLen);
+    // fill in padded
+    for (int y = 0; y < im.height; y++) {
+        for (int x = 0; x < im.width; x++) {
+            padded.data[(y+padLen)+(x+padLen)*padded.height] = im.data[y+x*im.height];
+        }
+    }
+    // nlm denoising
+    std::vector<float> inputA(7,1.0);
+    std::vector<float> inputB(7,3.0);
+    float ret;
+    switch (option) {
+        case 1:
+            std::cout<<"Running nlm_neon:"<<std::endl;
+            nlm_neon(N, K, h, padLen, threadCount, padded, output, C);
+            break;
+        case 0:
+        default:
+            std::cout<<"Running nlm_unoptimized:"<<std::endl;
+            nlm_unoptimized(N, K, h, padLen,threadCount,  padded, output, C);
+            break;
+    }
+
+    Image<uint8_t> result(im.height,im.width);
+    std::transform (
+            output.data.begin(),
+            output.data.end(),
+            C.data.begin(),
+            result.data.begin(),
+            [](auto i, auto j){
+                return std::floor(i/j*255.0);
+            });
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed_ms = 1e-6 * (end-start).count();
+    std::cout<<"Execution time ="<< elapsed_ms << " milliseconds\n";
+
+    // Write output
+    result.write("./denoised.ascii.pgm");
+
+    return 0;
+}
+
+void nlm_unoptimized(int N,
+                     int K,
+                     float h,
+                     int padLen,
+                     int threadCount,
+                     const Image<float> &padded,
+                     Image<float> &output,
+                     Image<float> &C) {
+    std::cout<<"Thread count ["<<threadCount<<"]\n";
+#pragma omp parallel for num_threads(threadCount)
+    for (int y = 0; y < output.height; y++) {
+        for (int x = 0; x < output.width; x++) {
+            for (int ny = -N; ny < N+1; ny++) {
+                for (int nx = -N; nx < N+1; nx++) {
+                    float ssd = 0.f;
+                    for (int ky = -K; ky < K + 1; ky++) {
+                        for (int kx = -K; kx < K + 1; kx++) {
+                            float diff;
+                            diff = padded.data[(padLen + y + ny + ky) + (padLen + x + nx + kx) * padded.height] -
+                                    padded.data[(padLen + y + ky) + (padLen + x + kx) * padded.height];
+                            ssd += (diff * diff);
+                        }
+                    }
+                    float dSquared = ssd/((2*K+1)*(2*K+1));
+                    float ex = std::exp(-dSquared/(h*h));
+                    C.data[y + x*C.height] += ex;
+                    output.data[y + x*output.height] += ex*padded.data[(padLen+y+ny) + (padLen+x+nx)*padded.height];
+                }
+            }
+        }
+    }
+}
+
+void nlm_neon(int N,
+              int K,
+              float h,
+              int padLen,
+              int threadCount,
+              const Image<float> &padded,
+              Image<float> &output,
+              Image<float> &C){
+    int kernelArea = (2*K+1)*(2*K+1);
+    std::cout<<"Thread count ["<<threadCount<<"]\n";
+#pragma omp parallel for num_threads(threadCount)
+    for (int y = 0; y < output.height; y++) {
+        for (int x = 0; x < output.width; x++) {
+            for (int ny = -N; ny < N+1; ny++) {
+                for (int nx = -N; nx < N+1; nx++) {
+                    float ssd = 0.f;
+                    // for kernel convolution, we iterate along the x-axis only,
+                    // at each iteration, process (2*K)+1 contiguous elements along y-axis
+                    for (int kx = -K; kx < K + 1; kx++) {
+                        int refIndex = (padLen + y + ny) + (padLen + x + nx + kx) * padded.height;
+                        int kernelIndex = (padLen + y) + (padLen + x + kx) * padded.height;
+                        ssd += ssd_reduce(padded.data.data()+kernelIndex, padded.data.data()+refIndex, 2*K+1);
+                    }
+                    float dSquared = ssd/kernelArea;
+                    float ex = std::exp(-dSquared/(h*h));
+                    C.data[y + x*C.height] += ex;
+                    output.data[y + x*output.height] += ex*padded.data[(padLen+y+ny) + (padLen+x+nx)*padded.height];
+                }
+            }
+        }
+    }
+}
